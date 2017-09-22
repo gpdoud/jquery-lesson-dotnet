f6e01724aea1b151b71021c1a090905e9d63448e
Greg doud <gpdoud@gmail.com>
2017-09-22 13:55:15 -0400

    Introduction to jQuery

---
+++ js/jquery.js
@@ -0,0 +1,28 @@
+$().ready(function() {
+
+	var InputTextIsRed = !true;
+
+	console.log("jQuery ready");
+
+	$("button").click(function() {
+		console.log("button clicked!");
+
+		// $("#buttonOut").val("Button click worked!!!");
+
+		var msg = $("#buttonOut").val();
+
+		$("#valueIn").val("GregD:" + msg);
+
+		if(InputTextIsRed) {
+			$("#valueIn").css("color", "red");
+		} else	{
+			$("#valueIn").css("color", "green"); 
+		}
+
+		InputTextIsRed = !InputTextIsRed;
+
+		$("ul").append("<li>" + msg + "</li>");
+
+	});
+
+});
